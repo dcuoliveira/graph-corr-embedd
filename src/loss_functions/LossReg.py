8b31b57333c8a29de00b8b5b76109895f427b71d
dcuoliveira <dan.cunhaa@gmail.com>
2023-11-01 06:45:27 -0300

    add condition

--- src/loss_functions/LossReg.py
+++ src/loss_functions/LossReg.py
@@ -5,10 +5,13 @@ class LossReg(nn.Module):
     def __init__(self):
         pass
 
-    def forward(self, nu1, nu2, model):
+    def forward(self, model):
 
         loss = 0
         for param in model.parameters():
-            L_reg += nu1 * torch.sum(torch.abs(param)) + nu2 * torch.sum(param * param)
+
+            if len(param.shape) != 1:
+                # loss += nu1 * torch.sum(torch.abs(param)) + nu2 * torch.sum(param * param)
+                loss += 1/2 * (torch.linalg.norm(param, 'fro') + torch.linalg.norm(param * param, 'fro'))
 
         return loss
