7ab3141f16a977cbbb9517e39e76fc6e80f0bf19
dcuoliveira <dan.cunhaa@gmail.com>
2023-11-25 09:12:43 -0300

    add comments

--- src/loss_functions/LossReg.py
+++ src/loss_functions/LossReg.py
@@ -6,12 +6,18 @@ class LossReg(nn.Module):
         pass
 
     def forward(self, model):
+        """"
+        Regularization loss function.
+        
+        :param model: model to be regularized
+        
+        :return: 1/2 * \sum_i ||w_i||^2
+        """
 
         loss = 0
         for param in model.parameters():
 
             if len(param.shape) != 1:
-                # loss += nu1 * torch.sum(torch.abs(param)) + nu2 * torch.sum(param * param)
-                loss += 1/2 * (torch.linalg.norm(param, 'fro') + torch.linalg.norm(param * param, 'fro'))
+                loss += torch.linalg.norm(param, 'fro')
 
-        return loss
+        return (1/2 * loss)
