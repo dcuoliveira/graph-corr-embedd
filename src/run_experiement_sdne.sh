dcfa569344000679cffa9bda9705d08edc92ce94
dcuoliveira <dan.cunhaa@gmail.com>
2023-11-28 21:17:14 -0300

    fix

--- src/run_experiement_sdne.sh
+++ src/run_experiement_sdne.sh
@@ -1,5 +1,5 @@
-python run_SDNE.py --model_name sdne-10ed --n_layers_enc 10 --n_layers_dec 10 &
-python run_SDNE.py --model_name sdne-20ed --n_layers_enc 20 --n_layers_dec 20 &
-python run_SDNE.py --model_name sdne-30ed --n_layers_enc 30 --n_layers_dec 30 &
-python run_SDNE.py --model_name sdne-40ed --n_layers_enc 40 --n_layers_dec 40 &
-python run_SDNE.py --model_name sdne-50ed --n_layers_enc 50 --n_layers_dec 50 &
+python run_SDNE.py --model_name sdne-10ed --n_layers_enc 10 --n_layers_dec 10
+python run_SDNE.py --model_name sdne-20ed --n_layers_enc 20 --n_layers_dec 20
+python run_SDNE.py --model_name sdne-30ed --n_layers_enc 30 --n_layers_dec 30
+python run_SDNE.py --model_name sdne-40ed --n_layers_enc 40 --n_layers_dec 40
+python run_SDNE.py --model_name sdne-50ed --n_layers_enc 50 --n_layers_dec 50
