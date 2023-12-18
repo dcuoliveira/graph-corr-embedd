5e603c3dddd94e8a62d6f3b8e2d2bcbb2415b1b2
maruan <maruanbakriottoni@gmail.com>
2023-12-18 18:56:03 -0300

    added fujita method

--- src/models/Spectrum.py
+++ src/models/Spectrum.py
@@ -1,17 +1,31 @@
-import torch
-import torch.nn as nn
 import numpy as np
+import torch.nn as nn
+from scipy.stats import spearmanr
 
 class Spectrum(nn.Module):
     def __init__(self):
         super(Spectrum, self).__init__()
 
     def forward(self, adj):
-
         if adj.shape[0] == adj.shape[1]:
-            eigenvalues, eigenvectors = np.linalg.eig(adj)
+            eigenvalues = np.linalg.eigvalsh(adj)
         else:
-            eigenvalues, eigenvectors = np.linalg.eig(adj @ adj.T)
+            eigenvalues = np.linalg.eigvalsh(adj @ adj.T)
+        return eigenvalues
 
-        return eigenvalues, eigenvectors
+    def largest_eigenvalue(self, adj):
+        eigenvalues = self.forward(adj)
+        return np.max(eigenvalues)
 
+    def compute_largest_eigenvalues(self, graphs):
+        return [self.largest_eigenvalue(g) for g in graphs]
+
+    def calculate_spearman_correlation(self, g1, g2):
+        largest_eigenvalues_g1 = self.compute_largest_eigenvalues(g1)
+        largest_eigenvalues_g2 = self.compute_largest_eigenvalues(g2)
+        correlation, _ = spearmanr(largest_eigenvalues_g1, largest_eigenvalues_g2)
+        return correlation
+
+
+if __name__ == "__main__":
+    pass
