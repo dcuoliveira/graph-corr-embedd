1ef5fcdcae0b108988bbbfcd6ca21ac865ab953d
dcuoliveira <dan.cunhaa@gmail.com>
2024-01-07 13:43:30 -0300

    change saving format

--- src/utils/conn_data.py
+++ src/utils/conn_data.py
@@ -1,14 +1,21 @@
 import pickle
+import joblib
 
 def save_pickle(path: str,
                 obj: dict):
-
-    with open(path, 'wb') as handle:
-        pickle.dump(obj, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    if path.endswith(".joblib"):
+        joblib.dump(obj, path)
+    else:
+        with open(path, 'wb') as handle:
+            pickle.dump(obj, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 def load_pickle(path: str):
 
-    with open(path, 'rb') as handle:
-        target_dict = pickle.load(handle)
+    if path.endswith(".joblib"):
+        target_dict = joblib.load(path)
+    else:
+        with open(path, 'rb') as handle:
+            target_dict = pickle.load(handle)
 
     return target_dict
