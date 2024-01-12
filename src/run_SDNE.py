95851baa28d950cc50aac8745792571d9c4a6c13
dcuoliveira <dan.cunhaa@gmail.com>
2024-01-11 21:52:45 -0300

    fix name

--- src/run_SDNE.py
+++ src/run_SDNE.py
@@ -169,6 +169,7 @@ if __name__ == '__main__':
     true = torch.tensor(true)
 
     results = {
+        "args": args,
         "pred": pred,
         "true": true,
         "train_total_loss": epochs_loss_train_tot,
@@ -177,8 +178,10 @@ if __name__ == '__main__':
         "train_reg_loss": epochs_loss_reg_tot,
     }
 
+    model_name = f'{args.model_name}_{int(args.n_hidden)}_{int(args.n_layers_enc)}_{int(args.n_layers_dec)}'
+
     # check if file exists
-    output_path = f"{os.path.dirname(__file__)}/data/outputs/{args.dataset_name}/{args.model_name}"
+    output_path = f"{os.path.dirname(__file__)}/data/outputs/{args.dataset_name}/{model_name}"
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
