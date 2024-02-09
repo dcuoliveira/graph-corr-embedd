1ee3896b1e551f555a9da6a20f09f4271d73fe91
dcuoliveira <dan.cunhaa@gmail.com>
2024-02-09 07:02:59 -0300

    fix graph loader

--- src/data/Simulation1Loader.py
+++ src/data/Simulation1Loader.py
@@ -1,13 +1,15 @@
 import os
+import sys
+
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+
 import torch
 from torch_geometric.utils.convert import from_networkx
 from torch_geometric.data import Data, DataLoader
 from torch_geometric.utils import from_networkx
 import networkx as nx
 import concurrent.futures
-
-import sys
-sys.path.append('../')
+import numpy as np
 
 from src.utils.conn_data import load_pickle
 
@@ -37,30 +39,35 @@ class Simulation1Loader(object):
     def create_graph_loader(self, batch_size: int=1):
         graph_data_list = []
 
-        for i, (tag, info) in enumerate(self.graph_data.items()):
-            graph1 = info['graph1']
-            graph2 = info['graph2']
-            target = info['cov']
+        for i, (cov_tag, graph_list) in enumerate(self.graph_data.items()):
+            cov_val = float(cov_tag)
 
-            # Convert NetworkX graphs to adjacency matrices
-            adj1 = torch.tensor(nx.adjacency_matrix(graph1).toarray())
-            adj2 = torch.tensor(nx.adjacency_matrix(graph2).toarray())
+            for n_sim, graph_pair_info in enumerate(graph_list):
 
-            # Use rows of adjacency matrices as features
-            x1 = adj1.type(torch.float32)
-            x2 = adj2.type(torch.float32)
+                graph1 = graph_pair_info['graph1']
+                graph2 = graph_pair_info['graph2']
 
-            # Concatenate the edge indices for both graphs
-            edge_index = torch.cat([from_networkx(graph1).edge_index, from_networkx(graph2).edge_index + graph1.number_of_nodes()], dim=1)
+                # Convert NetworkX graphs to adjacency matrices
+                adj1 = torch.tensor(nx.adjacency_matrix(graph1).toarray())
+                adj2 = torch.tensor(nx.adjacency_matrix(graph2).toarray())
 
-            # concatenate x1 and x2 creating a new dimension
-            x = torch.stack([x1, x2], dim=0)
+                # Use rows of adjacency matrices as features
+                x1 = adj1.type(torch.float32)
+                x2 = adj2.type(torch.float32)
 
+                # Concatenate the edge indices for both graphs
+                edge_index = torch.cat([from_networkx(graph1).edge_index, from_networkx(graph2).edge_index + graph1.number_of_nodes()], dim=1)
 
-            # Create a single Data object
-            data = Data(x=x, edge_index=edge_index, y=torch.tensor([target], dtype=torch.float))
+                # concatenate x1 and x2 creating a new dimension
+                x = torch.stack([x1, x2], dim=0)
 
-            graph_data_list.append(data)
+                if cov_val != np.round(graph_pair_info["cov"], 1):
+                    raise ValueError(f"Covariance value does not match: {cov_val}, {n_sim}")
+                
+                # Create a single Data object
+                data = Data(x=x, edge_index=edge_index, y=torch.tensor([cov_val], dtype=torch.float))
+
+                graph_data_list.append(data)
 
         # Create DataLoader
         loader = DataLoader(graph_data_list, batch_size=batch_size, shuffle=True)
@@ -110,7 +117,7 @@ if __name__ == "__main__":
 
         start = time.time()
 
-        loader = Simulation1Loader()
+        loader = Simulation1Loader(sample=True)
         graph_loader = loader.create_graph_loader()
 
         # time to minutes
