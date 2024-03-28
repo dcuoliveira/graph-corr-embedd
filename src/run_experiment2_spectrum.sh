f7ad51fbfe8f908b4743bcc2dafc06f13857f1f5
maruan <maruanbakriottoni@gmail.com>
2024-03-28 17:54:08 -0300

    added simulation 2 script

--- src/run_experiment2_spectrum.sh
+++ src/run_experiment2_spectrum.sh
@@ -1,5 +1,8 @@
 
 #!/bin/sh
 
-# initial experiment tets
-python3 run_simulattion2a.py --sample True --simulation_name simulation2a --graph_name erdos_renyi --n_simulations 30 --n_graphs 50 --n_nodes 100
+# test the simulation2a
+python3 run_simulation2a.py --sample False --simulation_name simulation2a --graph_types ["erdos_renyi", "random_geometric", "watts_strogatz"] --n_simulation 20 --n_graphs [10,20] --n_nodes 50 --covariance 0
+
+# test the simulation2b
+#python3 run_simulation2a.py --sample False --simulation_name simulation2a --graph_types ["erdos_renyi", "random_geometric", "watts_strogatz"] --n_simulation 20 --n_graphs [10,20] --n_nodes 50 --covariance 0.9
--- src/run_simulation2a.py
+++ src/run_simulation2a.py
@@ -2,85 +2,200 @@ import os
 import argparse
 import numpy as np
 from tqdm import tqdm
+from scipy.stats import spearmanr, norm
+import networkx as nx
+import matplotlib.pyplot as plt
 
 from simulation.GraphSim import GraphSim
 from utils.conn_data import save_pickle
 from utils.activation_functions import sigmoid
+
+from models.Spectrum import Spectrum
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--source_path', type=str, help='Source path for saving output.', default=os.path.dirname(__file__))
 parser.add_argument('--sample', type=str, help='Boolean if sample graph to save.', default=False)
 parser.add_argument('--simulation_name', type=str, help='Simulation name to be used on inputs dir.', default="simulation2a")
-parser.add_argument('--graph_name', type=str, help='Graph name to be generated.', default="erdos_renyi")
-
+parser.add_argument('--graph_types', type=np.array, help='Graph name to be generated.', default=["erdos_renyi", "random_geometric", "watts_strogatz"])
 parser.add_argument('--n_simulations', type=int, help='Number of simulations.', default=30)
-parser.add_argument('--n_graphs', type=int, help='Number of graphs per simulation.', default=50)
-parser.add_argument('--n_nodes', type=int, help='Number of nodes.', default=100)
+parser.add_argument('--n_graphs', type=np.array, help='Number of graphs per simulation.', default=[10,20])
+parser.add_argument('--n_nodes', type=float, help='Number of nodes.', default=25)
+parser.add_argument('--covariance', type=float, help='Covariance.', default=0)
+
+def get_spearm_pvalues_baseline(params, n_graph, first_family, second_family, n_simulations):
+    pvalues = []
+    for _ in range(n_simulations):
+        first_run = np.array(params[first_family][second_family][n_graph])[:,0]
+        second_run = np.array(params[first_family][second_family][n_graph])[:,1]
+        pvalues.append(spearmanr(first_run, second_run).pvalue)
+    return pvalues
+
+def get_spearman_pvalues(eigenvalues_dict, first_family_name, second_family_name, n_simulations, n_graph):
+    pvalues = []
+    for i in range(n_simulations):
+        first_family = eigenvalues_dict[first_family_name][second_family_name][n_graph][i][0]
+        second_family = eigenvalues_dict[first_family_name][second_family_name][n_graph][i][1]
+        pvalues.append(spearmanr(first_family, second_family).pvalue)
+    return pvalues
+
+def simulation_graph_case(gs, theta, graph_name, n_nodes):
+    if graph_name == "erdos_renyi":
+        graph = gs.simulate_erdos(n=n_nodes, prob=theta)
+    elif graph_name == "random_geometric":
+        graph = gs.simulate_geometric(n=n_nodes, radius=theta)
+    elif graph_name == "barabasi_albert":
+        graph = gs.simulate_barabasi_albert(n=n_nodes, m=int(10*theta))
+    elif graph_name == "watts_strogatz":
+        graph = gs.simulate_watts_strogatz(n=n_nodes, k=3, p=theta)
+    return graph
+
+def get_dicts(n_graphs, graph_classes):
+    embed_dict = {graph_name: {n_graph: [] for n_graph in n_graphs} for graph_name in graph_classes}
+    embed_dict = {graph_name: embed_dict for graph_name in graph_classes}
+    params_dict = {graph_name: {n_graph: [] for n_graph in n_graphs} for graph_name in graph_classes}
+    params_dict = {graph_name: params_dict for graph_name in graph_classes}
+    return embed_dict, params_dict
+
+def simulate_vector_graphs(n_graph, graph_name1, graph_name2, n_nodes, gs, p1, p2 ):
+    embeddings1, embeddings2 = [], []
+    for _ in range(n_graph):
+        # Generate parameters and normalize then gen graph
+        graph1  = simulation_graph_case(gs, p1, graph_name1, n_nodes=n_nodes)
+        graph2  = simulation_graph_case(gs, p2, graph_name2, n_nodes=n_nodes)
+        ##############
+        # Fujita method
+        # TODO: Try embedders
+        largest_eigenvalue1 = Spectrum().forward(nx.adjacency_matrix(graph1).A)
+        largest_eigenvalue2 = Spectrum().forward(nx.adjacency_matrix(graph2).A)
+        ############3##
+
+        embeddings1.append(largest_eigenvalue1)
+        embeddings2.append(largest_eigenvalue2)
+    return embeddings1, embeddings2
+
+def ensure_nested_dicts(embed_dict, params_dict, graph_name1, graph_name2, n_graph):
+    if graph_name1 not in embed_dict:
+        embed_dict[graph_name1] = {}
+    if graph_name2 not in embed_dict[graph_name1]:
+        embed_dict[graph_name1][graph_name2] = {}
+    if n_graph not in embed_dict[graph_name1][graph_name2]:
+        embed_dict[graph_name1][graph_name2][n_graph] = []
+    
+    if graph_name1 not in params_dict:
+        params_dict[graph_name1] = {}
+    if graph_name2 not in params_dict[graph_name1]:
+        params_dict[graph_name1][graph_name2] = {}
+    if n_graph not in params_dict[graph_name1][graph_name2]:
+        params_dict[graph_name1][graph_name2][n_graph] = []
+    
+    return embed_dict, params_dict
+
+def run_simulation(n_graphs, n_simulations, n_nodes, covariance, graph_classes):
+    embed_dict, params_dict = get_dicts(n_graphs, graph_classes)
+    simulated_combinations = set()
+
+    for n_graph in n_graphs:
+        for graph_name1 in tqdm(graph_classes, desc=f"Simulating {n_graph} graphs"):
+            for graph_name2 in tqdm(graph_classes, desc=f"Simulating {n_graph} graphs"):
+                
+                # Check if this combination or its reverse has been simulated
+                if (graph_name1, graph_name2, n_graph) in simulated_combinations or (graph_name2, graph_name1, n_graph) in simulated_combinations:
+                    continue  # Skip this combination if it has already been simulated
+                
+                # Mark this combination as simulated
+                simulated_combinations.add((graph_name1, graph_name2, n_graph))
+                gs = GraphSim(graph_name=graph_name1)  # It doesn't matter the graph_name here
+
+                #TODO: Shuoold the vector of params be here
+                for _ in range(n_simulations):
+                    #TODO: Should the vector of params be here
+                    gs.update_seed()
+                    theta = gs.get_p_from_bivariate_gaussian(s=covariance)
+                    theta = sigmoid(np.abs(theta))  # normalize
+                    theta1, theta2 = theta[0,0], theta[0,1]
+
+                    embeddings1, embeddings2  = simulate_vector_graphs(n_graph, graph_name1, graph_name2,
+                                                                       n_nodes, gs, theta1, theta2)
+
+                    embed_dict, params_dict = ensure_nested_dicts(embed_dict=embed_dict, params_dict=params_dict,
+                                                                  graph_name1=graph_name1, graph_name2=graph_name2,
+                                                                  n_graph=n_graph)
+
+                    # Store the results
+                    embed_dict[graph_name1][graph_name2][n_graph].append((embeddings1, embeddings2))
+                    params_dict[graph_name1][graph_name2][n_graph].append(theta[0])
+
+    return embed_dict, params_dict
+
+def plot_roc_curves(graph_types, n_graphs, eigen, params, n_simulations):
+    def calculate_rates(p_values, threshold):
+        fp, tn = 0, 0
+        for p_value in p_values:
+            if p_value > threshold:
+                fp += 1
+            else:
+                tn += 1
+        return tn / (fp + tn) if (fp + tn) > 0 else 0
+
+    fig, axs = plt.subplots(len(graph_types), len(graph_types), figsize=(15, 15))
+    for n_graph in n_graphs:
+        for i, gt1 in enumerate(graph_types):
+            for j, gt2 in enumerate(graph_types):
+                if j >= i:  # This condition ensures we only fill the upper triangle
+                    pval = get_spearman_pvalues(eigen, n_simulations=n_simulations, n_graph=n_graph,
+                                                first_family_name=gt1, second_family_name=gt2)
+                    thresholds = np.linspace(0, 1, len(pval))
+                    fprs = [calculate_rates(pval, th) for th in thresholds]
+                    
+                    axs[i, j].plot(thresholds, fprs, marker='.', label=f'{n_graph}')
+                    axs[i, j].plot([0, 1], [0, 1], linestyle='--')
+                    axs[i, j].set_xlabel('P-value Threshold')
+                    axs[i, j].set_title(f'{gt1} vs {gt2}')
+                    axs[i, j].legend()
+                else:
+                    axs[i, j].axis('off')
+
+    pval_baseline = get_spearm_pvalues_baseline(params, n_graph=n_graphs[-1],
+                                                first_family='erdos_renyi', second_family='erdos_renyi',
+                                                n_simulations=n_simulations)
+    fprs_baseline = [calculate_rates(pval_baseline, th) for th in thresholds]
+    axs[-1, 0].axis('on')
+    axs[-1, 0].plot(thresholds, fprs_baseline, marker='.', label=f'{n_graphs[-1]}')
+    axs[-1, 0].plot([0, 1], [0, 1], linestyle='--')
+    axs[-1, 0].set_xlabel('P-value Threshold')
+    axs[-1, 0].set_title('Baseline')
+    axs[-1, 0].legend()
+    plt.suptitle('Upper Triangle of ROC Curves Matrix', fontsize=16)
+    plt.tight_layout()
+    plt.legend()
+    plt.close()
+    return fig
 
 if __name__ == "__main__":
     args = parser.parse_args()
 
     # Check if path exists
-    output_path = f"{args.source_path}/data/inputs/{args.simulation_name}"
+    input_path = f"{args.source_path}/data/inputs/{args.simulation_name}"
+    output_path = f"{args.source_path}/data/outputs/{args.simulation_name}"
+
+    if not os.path.exists(input_path):
+        os.makedirs(input_path)
+
     if not os.path.exists(output_path):
         os.makedirs(output_path)
+
+    # Run the simulation
+    eigen, params = run_simulation(args.n_graphs, args.n_simulations, args.n_nodes, args.covariance, args.graph_types)
     
-    # generate covariance between gaussian random variables
-    graph_classes = [
-        "erdos_renyi",
-        "random_geometric",
-        "random_regular",
-        "barabasi_albert",
-        "watts_strogatz",
-    ]
-
-    # start simulation procedure
-    all_graphs = {}
-    for graph_name in tqdm(graph_classes, total=len(graph_classes), desc=f"Simulating graphs for {args.simulation_name}"):
-
-        # init graph sim class
-        gs = GraphSim(graph_name=graph_name)
-
-        graphs_given_cov = []
-        for i in range(args.n_simulations):
-
-            for j in range(args.n_graphs):
-
-                # gen seed
-                gs.update_seed()
-                save_seed = gs.seed
-
-                # generate probability of edge creation
-                p = gs.get_p_from_bivariate_gaussian(s=0)
-                p = sigmoid(p.__abs__())
-
-                if graph_name == "erdos_renyi":
-                    graph1 = gs.simulate_erdos(n=args.n_nodes, prob=p[0,0])
-                    graph2 = gs.simulate_erdos(n=args.n_nodes, prob=p[0,1])
-                elif graph_name == "random_geometric":
-                    graph1 = gs.simulate_random_geometric(n=args.n_nodes, prob=p[0,0])
-                    graph2 = gs.simulate_random_geometric(n=args.n_nodes, prob=p[0,1])
-
-                # save graph
-                sim_graph_info = {
-                    
-                    "n_simulations": i,
-                    "n_graphs": j,
-                    "graph1": graph1,
-                    "graph2": graph2,
-                    "seed": save_seed,
-                    "p": p,
-                    "cov": s
-                    
-                }
-
-                graphs_given_cov.append(sim_graph_info)
-
-        all_graphs[f"{np.round(s, 1)}"] = graphs_given_cov
-
     if not args.sample:
-        save_pickle(path=f"{output_path}/all_graph_info.pkl", obj=all_graphs)
+        save_pickle(path=f"{input_path}/eigenvalues_graphs_info.pkl", obj=eigen)
+        save_pickle(path=f"{input_path}/parameters_graphs_info.pkl", obj=params)
     else:
-        save_pickle(path=f"{output_path}/sample_graph_info.pkl", obj=all_graphs)
+        save_pickle(path=f"{input_path}/sample_eigenvalues_graphs_info.pkl", obj=eigen)
+        save_pickle(path=f"{input_path}/sample_parameters_graphs_info.pkl", obj=params)
+
+    fig = plot_roc_curves(args.graph_types, args.n_graphs, eigen, params, args.n_simulations)
+    fig.savefig(os.path.join(output_path, 'result.png'))
+
 
