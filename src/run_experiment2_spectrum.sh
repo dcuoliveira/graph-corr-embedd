67f9ce18936cc7d54d7e72665f6be08af4f203fd
maruan <maruanbakriottoni@gmail.com>
2024-03-28 17:55:48 -0300

    fixxx

--- src/run_experiment2_spectrum.sh
+++ src/run_experiment2_spectrum.sh
@@ -5,4 +5,4 @@
 python3 run_simulation2a.py --sample False --simulation_name simulation2a --graph_types ["erdos_renyi", "random_geometric", "watts_strogatz"] --n_simulation 20 --n_graphs [10,20] --n_nodes 50 --covariance 0
 
 # test the simulation2b
-#python3 run_simulation2a.py --sample False --simulation_name simulation2a --graph_types ["erdos_renyi", "random_geometric", "watts_strogatz"] --n_simulation 20 --n_graphs [10,20] --n_nodes 50 --covariance 0.9
+python3 run_simulation2a.py --sample False --simulation_name simulation2a --graph_types ["erdos_renyi", "random_geometric", "watts_strogatz"] --n_simulation 20 --n_graphs [10,20] --n_nodes 50 --covariance 0.6
