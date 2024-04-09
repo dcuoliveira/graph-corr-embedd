010c9aedb5d47f37031f55a7a1fb4221264c0a23
dcuoliveira <dan.cunhaa@gmail.com>
2024-04-09 16:55:45 -0300

    add activation functions

--- src/utils/activation_functions.py
+++ src/utils/activation_functions.py
@@ -2,3 +2,12 @@ import numpy as np
 
 def sigmoid(x):
  return 1/(1 + np.exp(-x))
+
+def min_max_normalization(x):
+    return (x - np.min(x)) / (np.max(x) - np.min(x))
+
+def standardization(x):
+    return (x - np.mean(x)) / np.std(x)
+
+def sum_normalization(x):
+    return x / np.sum(x)
