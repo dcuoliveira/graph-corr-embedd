646440098406f1f5ba5c0786a9c283355abef4d1
dcuoliveira <dan.cunhaa@gmail.com>
2024-04-14 12:23:21 -0500

    add experiment runner

--- src/run_experiment_sae.sh
+++ src/run_experiment_sae.sh
@@ -1,5 +1,2 @@
 
-python3 run_SAE.py --model_name sae --hidden_sizes 30,15,30 --dropout 0.5
-#python3 run_SAE.py --model_name sae --hidden_sizes 50,25,50 --dropout 0.3
-#python3 run_SAE.py --model_name sae --hidden_sizes 50,25,50 --dropout 0.5 --learning_rate 0.01
-#python3 run_SAE.py --model_name sae --hidden_sizes 50,25,50 --dropout 0.5 --sparsity_penalty 1e-3
+python run_SAE.py --model_name sae --dropout 0.5 --epochs 100 --hidden_sizes [30,15,30]
