05c81d415188c5c7e517e9ca1bffbfbdb16f7116
dcuoliveira <dan.cunhaa@gmail.com>
2024-04-14 10:05:17 -0500

    fix epochs name

--- src/run_SDNE.py
+++ src/run_SDNE.py
@@ -203,7 +203,7 @@ if __name__ == '__main__':
         "train_reg_loss": epochs_loss_reg_tot,
     }
 
-    model_name = f'{args.model_name}_{int(args.n_hidden)}_{int(args.n_layers_enc)}_{int(args.n_layers_dec)}_{int(args.epoch)}'
+    model_name = f'{args.model_name}_{int(args.n_hidden)}_{int(args.n_layers_enc)}_{int(args.n_layers_dec)}_{int(args.epochs)}'
 
     # check if file exists
     output_path = f"{os.path.dirname(__file__)}/data/outputs/{args.dataset_name}/{model_name}"
