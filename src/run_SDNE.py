8a05837354807f31ef5d8e77bb6c638ba0a1cefd
dcuoliveira <dan.cunhaa@gmail.com>
2024-04-18 09:05:49 -0500

    add losses

--- src/run_SDNE.py
+++ src/run_SDNE.py
@@ -10,6 +10,8 @@ from data.Simulation1Loader import Simulation1Loader
 from loss_functions.LossGlobal import LossGlobal
 from loss_functions.LossLocal import LossLocal
 from loss_functions.LossReg import LossReg
+from loss_functions.LossAbsDistance import LossAbsDistance
+from loss_functions.LossDistance import LossDistance
 
 from utils.conn_data import save_pickle
 from utils.parsers import str_2_bool
@@ -18,11 +20,12 @@ parser = argparse.ArgumentParser()
 
 # General parameters
 parser.add_argument('--dataset_name', type=str, help='Dataset name.', default="simulation1")
-parser.add_argument('--sample', type=str, help='Boolean if sample graph to save.', default=True)
+parser.add_argument('--sample', type=str, help='Boolean if sample graph to save.', default=False)
 parser.add_argument('--batch_size', type=int, help='Batch size to traint the model.', default=1)
 parser.add_argument('--model_name', type=str, help='Model name.', default="sdne")
 parser.add_argument('--n_nodes', type=int, help='Number of nodes.', default=100)
 parser.add_argument('--shuffle', type=str, help='Shuffle the dataset.', default=True)
+parser.add_argument('--loss_name', type=str, help='Additional loss to include in the sdne model.', default=None, choices=[None, "abs_distance", "distance"])
 
 parser.add_argument('--n_hidden', type=int, help='Number of hidden dimensions in the nn.', default=100)
 parser.add_argument('--n_layers_enc', type=int, help='Number of layers in the encoder network.', default=1)
@@ -32,14 +35,20 @@ parser.add_argument('--learning_rate', type=float, help='Learning rate of the op
 parser.add_argument('--epochs', type=int, help='Epochs to train the model.', default=10)
 parser.add_argument('--beta', default=5., type=float, help='beta is a hyperparameter in SDNE.')
 parser.add_argument('--alpha', type=float, default=1e-2, help='alpha is a hyperparameter in SDNE.')
+parser.add_argument('--gamma', type=float, default=1e3, help='gamma is a hyperparameter to multiply the add loss function.')
 parser.add_argument('--nu', type=float, default=1e-5, help='nu is a hyperparameter in SDNE.')
 
 if __name__ == '__main__':
 
+    # parse args
     args = parser.parse_args()
 
+    # convert to boolean
     args.sample = str_2_bool(args.sample)
     args.shuffle = str_2_bool(args.shuffle)
+
+    # fix model name
+    args.model_name = f"{args.model_name}_{args.loss_name}"
 
     # define dataset
     sim = Simulation1Loader(name=args.dataset_name, sample=args.sample)
@@ -71,17 +80,19 @@ if __name__ == '__main__':
     loss_local = LossLocal()
     loss_global = LossGlobal()
     loss_reg = LossReg()
+    loss_dis = LossDistance()
+    loss_abs_dis = LossAbsDistance()
 
     # initialize tqdm
     pbar = tqdm(range(args.epochs))
 
     train_results = []
     xs_train, zs_train, z_norms_train = [], [], []
-    epochs_loss_train_tot, epochs_loss_global_tot, epochs_loss_local_tot, epochs_loss_reg_tot = [], [], [], []
+    epochs_loss_train_tot, epochs_loss_global_tot, epochs_loss_local_tot, epochs_loss_reg_tot, epochs_loss_add = [], [], [], [], []
     for epoch in pbar:
 
-        loss_train_tot1, loss_global_tot1, loss_local_tot1, loss_reg_tot1 = 0, 0, 0, 0
-        loss_train_tot2, loss_global_tot2, loss_local_tot2, loss_reg_tot2 = 0, 0, 0, 0
+        loss_train_tot1, loss_global_tot1, loss_local_tot1, loss_reg_tot1, loss_add_tot1 = 0, 0, 0, 0, 0
+        loss_train_tot2, loss_global_tot2, loss_local_tot2, loss_reg_tot2, loss_add_tot2 = 0, 0, 0, 0, 0
         epoch_results = []
         for data in train_loader:
             # get inputs
@@ -107,14 +118,22 @@ if __name__ == '__main__':
             lg1 = loss_global.forward(adj=x1, x=x1_hat, b_mat=b1_mat)
             lr1 = loss_reg.forward(model=model1)
 
+            if args.loss_name == "abs_distance":
+                ladd1 = loss_abs_dis.forward(pred=pred_cov, true=data.y)
+            elif args.loss_name == "distance":
+                ladd1 = loss_dis.forward(pred=pred_cov, true=data.y)
+            else:
+                ladd1 = 0
+
             ## compute total loss
-            ## lg >>> lr > ll
-            lt1 = (args.alpha * lg1) + ll1 + (args.nu * lr1)
+            ## lg ~ ladd >>> lr > ll
+            lt1 = (args.alpha * lg1) + ll1 + (args.nu * lr1) + (args.gamma * ladd1)
 
             loss_train_tot1 += lt1
             loss_global_tot1 += lg1
             loss_local_tot1 += ll1
             loss_reg_tot1 += lr1
+            loss_add_tot1 += ladd1
 
             ## backward pass
             lt1.backward()
@@ -125,14 +144,22 @@ if __name__ == '__main__':
             lg2 = loss_global.forward(adj=x2, x=x2_hat, b_mat=b2_mat)
             lr2 = loss_reg.forward(model=model2)
 
+            if args.loss_name == "abs_distance":
+                ladd2 = loss_abs_dis.forward(pred=pred_cov, true=data.y)
+            elif args.loss_name == "distance":
+                ladd2 = loss_dis.forward(pred=pred_cov, true=data.y)
+            else:
+                ladd2 = 0
+
             ## compute total loss
-            ## lg >>> lr > ll
-            lt2 = (args.alpha * lg2) + ll2 + (args.nu * lr2)
+            ## g ~ ladd >>> lr > ll
+            lt2 = (args.alpha * lg2) + ll2 + (args.nu * lr2) + (args.gamma * ladd2)
 
             loss_train_tot2 += lt2
             loss_global_tot2 += lg2
             loss_local_tot2 += ll2
             loss_reg_tot2 += lr2
+            loss_add_tot2 += ladd2
 
             ## backward pass
             lt2.backward()
@@ -149,12 +176,14 @@ if __name__ == '__main__':
         epochs_loss_global_tot.append([loss_global_tot1.detach(), loss_train_tot2.detach()])
         epochs_loss_local_tot.append([loss_local_tot1.detach(), loss_train_tot2.detach()])
         epochs_loss_reg_tot.append([loss_reg_tot1.detach(), loss_train_tot2.detach()])
+        epochs_loss_add.append([loss_add_tot1.detach(), loss_add_tot2.detach()])
+
         train_results.append(epoch_results)
 
     # pred list to tensor
     train_results = torch.stack(train_results)
 
-    pbar = tqdm(sim.n_simulations, total=len(sim.n_simulations), desc="Running Spectrum model")
+    pbar = tqdm(sim.n_simulations, total=len(sim.n_simulations), desc="Running SDNE model on Test Data")
     test_results = []
     with torch.no_grad():
         for n in pbar:
