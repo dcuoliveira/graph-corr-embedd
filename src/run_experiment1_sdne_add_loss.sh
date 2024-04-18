587b2b586d89029cb84fa7e5a25d65677a52907b
dcuoliveira <dan.cunhaa@gmail.com>
2024-04-18 09:05:03 -0500

    first commit

---
+++ src/run_experiment1_sdne_add_loss.sh
@@ -0,0 +1,13 @@
+python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 1 --n_layers_dec 1 --epochs 10 --loss_name abs_distance
+python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 10 --n_layers_dec 10 --epochs 10 --loss_name abs_distance
+python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 1 --n_layers_dec 1 --epochs 100 --loss_name abs_distance
+python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 10 --n_layers_dec 10 --epochs 100 --loss_name abs_distance
+python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 1 --n_layers_dec 1 --epochs 1000 --loss_name abs_distance
+python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 10 --n_layers_dec 10 --epochs 1000 --loss_name abs_distance
+
+python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 1 --n_layers_dec 1 --epochs 10 --loss_name distance
+python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 10 --n_layers_dec 10 --epochs 10 --loss_name distance
+python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 1 --n_layers_dec 1 --epochs 100 --loss_name distance
+python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 10 --n_layers_dec 10 --epochs 100 --loss_name distance
+python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 1 --n_layers_dec 1 --epochs 1000 --loss_name distance
+python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 10 --n_layers_dec 10 --epochs 1000 --loss_name distance
