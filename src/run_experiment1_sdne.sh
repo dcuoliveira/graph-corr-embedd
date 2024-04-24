b1409a8d31fca084bae83b4d33f3b856c0936367
dcuoliveira <dan.cunhaa@gmail.com>
2024-04-24 06:13:15 -0300

    add commnets

--- src/run_experiment1_sdne.sh
+++ src/run_experiment1_sdne.sh
@@ -1,4 +1,4 @@
-# python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 1 --n_layers_dec 1 --epochs 100
-# python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 10 --n_layers_dec 10 --epochs 100
-python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 1 --n_layers_dec 1 --epochs 500
-python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 10 --n_layers_dec 10 --epochs 500
+python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 1 --n_layers_dec 1 --epochs 100
+python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 10 --n_layers_dec 10 --epochs 100
+# python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 1 --n_layers_dec 1 --epochs 500
+# python run_SDNE.py --model_name sdne --sample False --n_hidden 100 --n_layers_enc 10 --n_layers_dec 10 --epochs 500
