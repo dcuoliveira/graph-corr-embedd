1eb93ace3e5af57e34d6e78329a8a74429ba282e
dcuoliveira <dan.cunhaa@gmail.com>
2024-05-10 06:37:24 -0300

    add sdne v1

--- src/run_SDNE1.py
+++ src/run_SDNE1.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 from torch_geometric.data import DataLoader
 
 from models.SDNE import SDNE
-from src.data.Simulation1aLoader import Simulation1aLoader
+from data.Simulation1aLoader import Simulation1aLoader
 from loss_functions.LossGlobal import LossGlobal
 from loss_functions.LossLocal import LossLocal
 from loss_functions.LossReg import LossReg
@@ -19,10 +19,10 @@ from utils.parsers import str_2_bool
 parser = argparse.ArgumentParser()
 
 # General parameters
-parser.add_argument('--dataset_name', type=str, help='Dataset name.', default="simulation1")
+parser.add_argument('--dataset_name', type=str, help='Dataset name.', default="simulation1a")
 parser.add_argument('--sample', type=str, help='Boolean if sample graph to save.', default=False)
-parser.add_argument('--batch_size', type=int, help='Batch size to traint the model.', default=1)
-parser.add_argument('--model_name', type=str, help='Model name.', default="sdne-new2")
+parser.add_argument('--batch_size', type=int, help='Batch size to traint the model.', default=2)
+parser.add_argument('--model_name', type=str, help='Model name.', default="sdne1")
 parser.add_argument('--n_nodes', type=int, help='Number of nodes.', default=100)
 parser.add_argument('--shuffle', type=str, help='Shuffle the dataset.', default=True)
 
@@ -31,6 +31,7 @@ parser.add_argument('--n_layers_enc', type=int, help='Number of layers in the en
 parser.add_argument('--n_layers_dec', type=int, help='Number of layers in the decoder network.', default=1)
 parser.add_argument('--dropout', type=float, help='Dropout rate (1 - keep probability).', default=0.5)
 parser.add_argument('--learning_rate', type=float, help='Learning rate of the optimization algorithm.', default=0.001)
+parser.add_argument('--epochs', type=int, help='Epochs to train the model.', default=10)
 parser.add_argument('--beta', default=5., type=float, help='beta is a hyperparameter in SDNE.')
 parser.add_argument('--alpha', type=float, default=1e-2, help='alpha is a hyperparameter in SDNE.')
 parser.add_argument('--gamma', type=float, default=1e3, help='gamma is a hyperparameter to multiply the add loss function.')
@@ -45,12 +46,10 @@ if __name__ == '__main__':
     args.sample = str_2_bool(args.sample)
     args.shuffle = str_2_bool(args.shuffle)
 
-    # fix model name
-    args.model_name = f"{args.model_name}_{args.loss_name}"
-
     # define dataset
-    sim = Simulation1Loader(name=args.dataset_name, sample=args.sample)
-    dataset_list = sim.create_graph_list()
+    sim = Simulation1aLoader(name=args.dataset_name, sample=args.sample)
+    train_loader = sim.create_graph_loader(batch_size=args.batch_size)
+    test_dataset_list = sim.create_graph_list()
 
     # define model
     model1 = SDNE(node_size=args.n_nodes,
@@ -81,28 +80,27 @@ if __name__ == '__main__':
     loss_abs_dis = LossAbsDistance()
 
     # initialize tqdm
-    pbar = tqdm(sim.n_simulations, total=len(sim.n_simulations), desc="Running SDNE-NEW model")
+    pbar = tqdm(range(args.epochs))
 
-    train_results = []
-    xs_train, zs_train, z_norms_train = [], [], []
-    epochs_loss_train_tot, epochs_loss_global_tot, epochs_loss_local_tot, epochs_loss_reg_tot = [], [], [], []
+    epochs_tot_loss, epochs_global_loss, epochs_local_loss, epochs_reg_loss = [], [], [], []
+    epochs_predictions = []
     for epoch in pbar:
 
-        epoch_results = []
-        for cov in sim.covs:
+        opt1.zero_grad()  
+        opt2.zero_grad()
 
-            opt1.zero_grad()
-            opt2.zero_grad()
+        batch_tot_loss1, batch_global_loss1, batch_local_loss1, batch_reg_loss1 = [], [], [], []
+        batch_tot_loss2, batch_global_loss2, batch_local_loss2, batch_reg_loss2 = [], [], [], []
+        batch_predictions = []
+        for batch in train_loader:
 
-            filtered_data_list = [data for data in dataset_list if (data.n_simulations == epoch) and (data.y.item() == cov)]
-            filtered_loader = DataLoader(filtered_data_list, batch_size=args.batch_size, shuffle=args.shuffle)
-        
-            loss_train_tot1, loss_global_tot1, loss_local_tot1, loss_reg_tot1, loss_add_tot1 = 0, 0, 0, 0, 0
-            loss_train_tot2, loss_global_tot2, loss_local_tot2, loss_reg_tot2, loss_add_tot2 = 0, 0, 0, 0, 0
-            for data in filtered_loader:
+            lt1_tot, ll1_tot, lg1_tot, lr1_tot = 0, 0, 0, 0
+            lt2_tot, ll2_tot, lg2_tot, lr2_tot = 0, 0, 0, 0
+            for i, j in enumerate(range(0, args.batch_size * 2, 2)):
+            
                 # get inputs
-                x1 = data.x[0, :, :]
-                x2 = data.x[1, :, :]
+                x1 = batch.x[j, :, :]
+                x2 = batch.x[j+1, :, :]
 
                 # create global loss parameter matrix
                 b1_mat, b2_mat = torch.ones_like(x1), torch.ones_like(x2)
@@ -116,67 +114,75 @@ if __name__ == '__main__':
                 pred_cov = model1.compute_spearman_rank_correlation(x=z1.flatten().detach(), y=z2.flatten().detach())
 
                 # store pred and true values
-                epoch_results.append([pred_cov, data.y])
+                batch_predictions.append([pred_cov, batch[i].y])
 
                 # compute loss functions I
                 ll1 = loss_local.forward(adj=x1, z=z1)
                 lg1 = loss_global.forward(adj=x1, x=x1_hat, b_mat=b1_mat)
                 lr1 = loss_reg.forward(model=model1)
 
+                ll1_tot += ll1
+                lg1_tot += lg1
+                lr1_tot += lr1
+
                 ## compute total loss
                 ## lg ~ ladd >>> lr > ll
                 lt1 = (args.alpha * lg1) + ll1 + (args.nu * lr1)
 
-                loss_train_tot1 += lt1
-                loss_global_tot1 += lg1
-                loss_local_tot1 += ll1
-                loss_reg_tot1 += lr1
+                lt1_tot += lt1
 
                 # compute loss functions II
                 ll2 = loss_local.forward(adj=x2, z=z2)
                 lg2 = loss_global.forward(adj=x2, x=x2_hat, b_mat=b2_mat)
                 lr2 = loss_reg.forward(model=model2)
 
+                ll2_tot += ll2
+                lg2_tot += lg2
+                lr2_tot += lr2
+
                 ## compute total loss
                 ## g ~ ladd >>> lr > ll
                 lt2 = (args.alpha * lg2) + ll2 + (args.nu * lr2)
 
-                loss_train_tot2 += lt2
-                loss_global_tot2 += lg2
-                loss_local_tot2 += ll2
-                loss_reg_tot2 += lr2
+                lt2_tot += lt2
 
-                ## backward pass
-                lt1.backward()
-                opt1.step()
+            ## backward pass
+            lt1_tot.backward()
+            opt1.step()
+                
+            ## backward pass
+            lt2_tot.backward()
+            opt2.step()
 
-                ## backward pass
-                lt2.backward()
-                opt2.step()
+            batch_tot_loss1.append(lt1_tot.detach().item())
+            batch_global_loss1.append(lg1_tot.detach().item())
+            batch_local_loss1.append(ll1_tot.detach().item())
+            batch_reg_loss1.append(lr1_tot.detach().item())
 
-        epoch_results = torch.tensor(epoch_results)
+            batch_tot_loss2.append(lt2_tot.detach().item())
+            batch_global_loss2.append(lg2_tot.detach().item())
+            batch_local_loss2.append(ll2_tot.detach().item())
+            batch_reg_loss2.append(lr2_tot.detach().item())
 
+        epochs_predictions.append(torch.tensor(batch_predictions))
+
+        epochs_tot_loss.append(torch.stack([torch.tensor(batch_tot_loss1), torch.tensor(batch_tot_loss2)], axis=1))
+        epochs_global_loss.append(torch.stack([torch.tensor(batch_global_loss1), torch.tensor(batch_global_loss2)], axis=1))
+        epochs_local_loss.append(torch.stack([torch.tensor(batch_local_loss1), torch.tensor(batch_local_loss2)], axis=1))
+        epochs_reg_loss.append(torch.stack([torch.tensor(batch_reg_loss1), torch.tensor(batch_reg_loss2)], axis=1))
+        
         # update tqdm
         pbar.update(1)
-        pbar.set_description("Train Epoch: %d, Train Loss I & II: %.4f & %.4f" % (epoch, loss_train_tot1, loss_train_tot2))
-
-        # save loss
-        epochs_loss_train_tot.append([loss_train_tot1.detach(), loss_train_tot2.detach()])
-        epochs_loss_global_tot.append([loss_global_tot1.detach(), loss_global_tot2.detach()])
-        epochs_loss_local_tot.append([loss_local_tot1.detach(), loss_local_tot2.detach()])
-        epochs_loss_reg_tot.append([loss_reg_tot1.detach(), loss_reg_tot2.detach()])
-
-        if loss_add_tot1 != 0 and loss_add_tot2 != 0:
-            epochs_loss_add.append([loss_add_tot1.detach(), loss_add_tot2.detach()])
-        else:
-            epochs_loss_add.append([loss_add_tot1, loss_add_tot2])
-
-        train_results.append(epoch_results)
+        pbar.set_description("SDNE Train Epoch: %d, Train Loss I & II: %.4f & %.4f" % (epoch, batch_tot_loss1[-1], batch_tot_loss2[-1]))
 
     # pred list to tensor
-    train_results = torch.stack(train_results)
+    epochs_predictions = torch.stack(epochs_predictions)
+    epochs_tot_loss = torch.stack(epochs_tot_loss)
+    epochs_global_loss = torch.stack(epochs_global_loss)
+    epochs_local_loss = torch.stack(epochs_local_loss)
+    epochs_reg_loss = torch.stack(epochs_reg_loss)
 
-    pbar = tqdm(sim.n_simulations, total=len(sim.n_simulations), desc="Running SDNE-NEW model on Test Data")
+    pbar = tqdm(sim.n_simulations, total=len(sim.n_simulations), desc="Running SDNE model on Test Data")
     test_results = []
     with torch.no_grad():
         for n in pbar:
@@ -184,7 +190,7 @@ if __name__ == '__main__':
             simulation_results = []
             for cov in sim.covs:
 
-                filtered_data_list = [data for data in dataset_list if (data.n_simulations == n) and (data.y.item() == cov)]
+                filtered_data_list = [data for data in test_dataset_list if (data.n_simulations == n) and (data.y.item() == cov)]
                 filtered_loader = DataLoader(filtered_data_list, batch_size=args.batch_size, shuffle=args.shuffle)
 
                 embeddings = [] 
@@ -218,12 +224,12 @@ if __name__ == '__main__':
 
     results = {
         "args": args,
-        "train_results": train_results,
-        "test_results": test_results,
-        "train_total_loss": epochs_loss_train_tot,
-        "train_local_loss": epochs_loss_local_tot,
-        "train_global_loss": epochs_loss_global_tot,
-        "train_reg_loss": epochs_loss_reg_tot,
+        "train_predictions": epochs_predictions,
+        "test_predictions": test_results,
+        "train_loss": epochs_tot_loss,
+        "epochs_global_loss": epochs_global_loss,
+        "epochs_local_loss": epochs_local_loss,
+        "epochs_reg_loss": epochs_reg_loss,
     }
 
     model_name = f'{args.model_name}_{int(args.n_hidden)}_{int(args.n_layers_enc)}_{int(args.n_layers_dec)}_{int(args.epochs)}'
@@ -236,5 +242,9 @@ if __name__ == '__main__':
     # save file
     if args.sample:
         save_pickle(path=f"{output_path}/sample_results.pkl", obj=results)
+        torch.save(model1.state_dict(), f"{output_path}/model1_sample.pth")
+        torch.save(model2.state_dict(), f"{output_path}/model2_sample.pth")
     else:
         save_pickle(path=f"{output_path}/results.pkl", obj=results)
+        torch.save(model1.state_dict(), f"{output_path}/model1.pth")
+        torch.save(model2.state_dict(), f"{output_path}/model2.pth")
