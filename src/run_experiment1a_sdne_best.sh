a3b612e45a356246ddcae1539e286447e092835e
dcuoliveira <dan.cunhaa@gmail.com>
2024-06-09 19:33:39 -0300

    add best sdne

---
+++ src/run_experiment1a_sdne_best.sh
@@ -0,0 +1,5 @@
+python run_SDNE6.py --model_name sdne6 --sample False --batch_size 1 --n_hidden 20 --n_layers_enc 1 --n_layers_dec 1 --epochs 100000
+python run_SDNE6.py --model_name sdne6 --sample False --batch_size 1 --n_hidden 40 --n_layers_enc 1 --n_layers_dec 1 --epochs 100000
+python run_SDNE3.py --model_name sdne3 --sample False --batch_size 1 --n_hidden 30 --n_layers_enc 1 --n_layers_dec 1 --epochs 100000
+python run_SDNE3.py --model_name sdne3 --sample False --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 100000
+python run_SDNE3.py --model_name sdne3 --sample False --batch_size 1 --n_hidden 100 --n_layers_enc 1 --n_layers_dec 1 --epochs 100000
