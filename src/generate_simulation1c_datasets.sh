2adced29b6119b2daad2809054bd3adacfaf6c7d
dcuoliveira <dan.cunhaa@gmail.com>
2024-07-10 14:47:54 -0300

    first commit

---
+++ src/generate_simulation1c_datasets.sh
@@ -0,0 +1,5 @@
+python run_simulation1c.py --sample False --simulation_name simulation1c --graph_name erdos_renyi
+python run_simulation1c.py --sample False --simulation_name simulation1c --graph_name watts_strogatz
+python run_simulation1c.py --sample False --simulation_name simulation1c --graph_name k_regular
+python run_simulation1c.py --sample False --simulation_name simulation1c --graph_name geometric
+python run_simulation1c.py --sample False --simulation_name simulation1c --graph_name barabasi_albert
