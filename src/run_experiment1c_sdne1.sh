aa154b4741de343d70c6872c129bd2b2916c3a23
dcuoliveira <dan.cunhaa@gmail.com>
2024-07-11 14:49:54 -0300

    first commit

---
+++ src/run_experiment1c_sdne1.sh
@@ -0,0 +1,6 @@
+python run_SDNE1.py --model_name sdne1 --sample False --dataset_name simulation1c --graph_name erdos_renyi --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 100
+python run_SDNE1.py --model_name sdne1 --sample False --dataset_name simulation1c --graph_name erdos_renyi --batch_size 1 --n_hidden 100 --n_layers_enc 1 --n_layers_dec 1 --epochs 100
+python run_SDNE1.py --model_name sdne1 --sample False --dataset_name simulation1c --graph_name erdos_renyi --batch_size 1 --n_hidden 100 --n_layers_enc 10 --n_layers_dec 10 --epochs 100
+python run_SDNE1.py --model_name sdne1 --sample False --dataset_name simulation1c --graph_name erdos_renyi --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 200
+python run_SDNE1.py --model_name sdne1 --sample False --dataset_name simulation1c --graph_name erdos_renyi --batch_size 1 --n_hidden 100 --n_layers_enc 1 --n_layers_dec 1 --epochs 200
+python run_SDNE1.py --model_name sdne1 --sample False --dataset_name simulation1c --graph_name erdos_renyi --batch_size 1 --n_hidden 100 --n_layers_enc 10 --n_layers_dec 10 --epochs 200
