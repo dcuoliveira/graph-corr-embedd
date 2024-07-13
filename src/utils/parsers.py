06720a6beeb3717d7d04c9cb4e09eec2c50b4a1b
dcuoliveira <dan.cunhaa@gmail.com>
2024-07-13 20:53:03 -0300

    add split

--- src/utils/parsers.py
+++ src/utils/parsers.py
@@ -1,4 +1,25 @@
 import dgl
+import numpy as np
+
+def split_list_of_datasets(datasets: list, train_ratio: float = 0.8, val_ratio: float = 0.1):
+    n = len(datasets)
+    train_size = int(train_ratio * n)
+    val_size = int(val_ratio * n)
+    
+    train_idx = np.random.random_integers(0, n, size=train_size)
+    train_set = set(train_idx)
+    all_indices = set(range(n))
+    available_indices = all_indices - train_set
+    available_indices = list(available_indices)
+    val_idx = np.random.choice(available_indices, size=val_size, replace=False)
+    available_indices = all_indices - set(val_idx) - train_set
+    test_idx = np.array(list(available_indices))
+    
+    train_list = [datasets[i] for i in train_idx]
+    val_list = [datasets[i] for i in val_idx]
+    test_list = [datasets[i] for i in test_idx]
+
+    return train_list, val_list, test_list
 
 def tensor_to_dgl_graph_with_features(adj, node_features):
     # Get the source and destination nodes from the adjacency matrix
