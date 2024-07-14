c2cbc9cf5ca6c61885b02254400ee55e7e359b46
dcuoliveira <dan.cunhaa@gmail.com>
2024-07-14 08:30:39 -0300

    refactor

---
+++ src/run_experiment1c_spectrum.sh
@@ -0,0 +1,5 @@
+python run_Spectrum.py  --sample False --model_name spectrum --sample False --dataset_name simulation1c --graph_name erdos_renyi
+python run_Spectrum.py  --sample False --model_name spectrum --sample False --dataset_name simulation1c --graph_name watts_strogatz
+python run_Spectrum.py  --sample False --model_name spectrum --sample False --dataset_name simulation1c --graph_name k_regular
+python run_Spectrum.py  --sample False --model_name spectrum --sample False --dataset_name simulation1c --graph_name geometric
+python run_Spectrum.py  --sample False --model_name spectrum --sample False --dataset_name simulation1c --graph_name barabasi_albert
