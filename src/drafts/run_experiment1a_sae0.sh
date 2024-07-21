bd8eb82ac8b5c7261eae3a23990dd76f75fe9b26
dcuoliveira <dan.cunhaa@gmail.com>
2024-07-21 16:38:04 -0300

    results

---
+++ src/drafts/run_experiment1a_sae0.sh
@@ -0,0 +1,5 @@
+
+python run_SAE0.py --dataset_name simulation1a --model_name sae0 --sample False --input_size 100 --dropout 0.5 --epochs 10 --learning_rate 0.001 --sparsity_penalty 1e-4 --hidden_sizes "20,20"
+python run_SAE0.py --dataset_name simulation1a --model_name sae0 --sample False --input_size 100 --dropout 0.5 --epochs 10 --learning_rate 0.001 --sparsity_penalty 1e-4 --hidden_sizes "30,30"
+python run_SAE0.py --dataset_name simulation1a --model_name sae0 --sample False --input_size 100 --dropout 0.5 --epochs 10 --learning_rate 0.001 --sparsity_penalty 1e-4 --hidden_sizes "50,50"
+python run_SAE0.py --dataset_name simulation1a --model_name sae0 --sample False --input_size 100 --dropout 0.5 --epochs 10 --learning_rate 0.001 --sparsity_penalty 1e-4 --hidden_sizes "100,100"
