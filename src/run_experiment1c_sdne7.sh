46bc728ba3aeb5b8442c85c2d473834eb0378313
dcuoliveira <dan.cunhaa@gmail.com>
2024-07-21 17:30:54 -0300

    fix runs

--- src/run_experiment1c_sdne7.sh
+++ src/run_experiment1c_sdne7.sh
@@ -1,7 +1,7 @@
-python run_SDNE7.py --model_name sdne7 --sample False --dataset_name simulation1c --graph_name erdos_renyi --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 10
-python run_SDNE7.py --model_name sdne7 --sample False --dataset_name simulation1c --graph_name watts_strogatz --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 10
-python run_SDNE7.py --model_name sdne7 --sample False --dataset_name simulation1c --graph_name erdos_renyi --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 100
-python run_SDNE7.py --model_name sdne7 --sample False --dataset_name simulation1c --graph_name watts_strogatz --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 100
+# python run_SDNE7.py --model_name sdne7 --sample False --dataset_name simulation1c --graph_name erdos_renyi --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 10
+# python run_SDNE7.py --model_name sdne7 --sample False --dataset_name simulation1c --graph_name watts_strogatz --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 10
+# python run_SDNE7.py --model_name sdne7 --sample False --dataset_name simulation1c --graph_name erdos_renyi --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 100
+# python run_SDNE7.py --model_name sdne7 --sample False --dataset_name simulation1c --graph_name watts_strogatz --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 100
 python run_SDNE7.py --model_name sdne7 --sample False --dataset_name simulation1c --graph_name erdos_renyi --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 200
 python run_SDNE7.py --model_name sdne7 --sample False --dataset_name simulation1c --graph_name watts_strogatz --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 200
 python run_SDNE7.py --model_name sdne7 --sample False --dataset_name simulation1c --graph_name erdos_renyi --batch_size 1 --n_hidden 100 --n_layers_enc 1 --n_layers_dec 1 --epochs 10
--- src/run_experiment1c_sdne8.sh
+++ src/run_experiment1c_sdne8.sh
@@ -1,5 +1,5 @@
-# python run_SDNE8.py --model_name sdne8 --sample False --dataset_name simulation1c --graph_name erdos_renyi --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 10
-# python run_SDNE8.py --model_name sdne8 --sample False --dataset_name simulation1c --graph_name watts_strogatz --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 10
+python run_SDNE8.py --model_name sdne8 --sample False --dataset_name simulation1c --graph_name erdos_renyi --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 10
+python run_SDNE8.py --model_name sdne8 --sample False --dataset_name simulation1c --graph_name watts_strogatz --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 10
 python run_SDNE8.py --model_name sdne8 --sample False --dataset_name simulation1c --graph_name erdos_renyi --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 100
 python run_SDNE8.py --model_name sdne8 --sample False --dataset_name simulation1c --graph_name watts_strogatz --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 100
 python run_SDNE8.py --model_name sdne8 --sample False --dataset_name simulation1c --graph_name erdos_renyi --batch_size 1 --n_hidden 50 --n_layers_enc 1 --n_layers_dec 1 --epochs 200
