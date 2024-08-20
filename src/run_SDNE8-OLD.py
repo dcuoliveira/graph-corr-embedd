ce0d4f4e5e49f8046a6f70803304eb548ac81217
dcuoliveira <dan.cunhaa@gmail.com>
2024-08-20 10:25:41 -0300

    fixx param

--- src/run_SDNE8-OLD.py
+++ src/run_SDNE8-OLD.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
     elif args.dataset_name == "simulation1c":
         sim = Simulation1cLoader(name=args.dataset_name, sample=args.sample, graph_name = args.graph_name)
         print('Loading the simulation data!')
-        dataset_list = sim.create_graph_list(load_preprocessed=True)
+        dataset_list = sim.create_graph_list(load_preprocessed=False)
     else:
         raise Exception('Dataset not found!')
     print('Finish Loading')
