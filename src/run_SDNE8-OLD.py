d5116dd11cee8d435cb2e4e69cb5a108fb871943
dcuoliveira <dan.cunhaa@gmail.com>
2024-08-20 10:14:14 -0300

    fix old

--- src/run_SDNE8-OLD.py
+++ src/run_SDNE8-OLD.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
         dataset_list = sim.create_graph_list()
 
     elif args.dataset_name == "simulation1c":
-        sim = Simulation1cLoader(name=args.dataset_name, sample=args.sample, graph_name = args.graph_name, preprocessed=False)
+        sim = Simulation1cLoader(name=args.dataset_name, sample=args.sample, graph_name = args.graph_name)
         print('Loading the simulation data!')
         dataset_list = sim.create_graph_list(load_preprocessed=True)
     else:
