4aabe796a7bf16b52588451752eff72bdc0974b2
dcuoliveira <dan.cunhaa@gmail.com>
2024-11-04 15:59:53 +0000

    add parameter to represent the number og eigenvalues to get

--- src/models/Spectrum.py
+++ src/models/Spectrum.py
@@ -8,13 +8,16 @@ class Spectrum(Stats):
     def __init__(self):
         super(Spectrum, self).__init__()
 
-    def forward(self, adj):
+    def forward(self, adj, k=-1):
         if adj.shape[0] == adj.shape[1]:
             eigenvalues = np.linalg.eigvalsh(adj)
         else:
             eigenvalues = np.linalg.eigvalsh(adj @ adj.T)
 
-        max_eigenvalue = np.max(eigenvalues)
+        if k == -1:
+            max_eigenvalue = np.max(eigenvalues)
+        else:
+            max_eigenvalue = np.sort(eigenvalues)[-k:]
 
         return max_eigenvalue
 
