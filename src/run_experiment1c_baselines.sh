9dfd27792c2cf9570cc66b387b5533f19c21ef04
dcuoliveira <dan.cunhaa@gmail.com>
2024-11-23 06:55:54 -0300

    add load_preprocessd args to runner

--- src/run_experiment1c_baselines.sh
+++ src/run_experiment1c_baselines.sh
@@ -1,6 +1,6 @@
-python run_DeltaCon.py --model_name deltacon --sample False --dataset_name simulation1c --graph_name erdos_renyi
-python run_DeltaCon.py --model_name deltacon --sample False --dataset_name simulation1c --graph_name watts_strogatz
-python run_Frobenius.py --model_name frobenius --sample False --dataset_name simulation1c --graph_name erdos_renyi
-python run_Frobenius.py --model_name frobenius --sample False --dataset_name simulation1c --graph_name watts_strogatz
-python run_Procustes.py --model_name procustes --sample False --dataset_name simulation1c --graph_name erdos_renyi
-python run_Procustes.py --model_name procustes --sample False --dataset_name simulation1c --graph_name watts_strogatz
+python run_DeltaCon.py --model_name deltacon --sample False --load_preprocessed False --dataset_name simulation1c --graph_name erdos_renyi
+python run_DeltaCon.py --model_name deltacon --sample False --load_preprocessed False --dataset_name simulation1c --graph_name watts_strogatz
+python run_Frobenius.py --model_name frobenius --sample False --load_preprocessed True --dataset_name simulation1c --graph_name erdos_renyi
+python run_Frobenius.py --model_name frobenius --sample False --load_preprocessed True --dataset_name simulation1c --graph_name watts_strogatz
+python run_Procustes.py --model_name procustes --sample False --load_preprocessed True --dataset_name simulation1c --graph_name erdos_renyi
+python run_Procustes.py --model_name procustes --sample False --load_preprocessed True --dataset_name simulation1c --graph_name watts_strogatz
